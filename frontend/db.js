57246ceb7a086b0f084b590a556cca620954805d
pankhuri26-ship-it <pankhuri2917@gmail.com>
2026-02-08 05:54:48 +0530

    update

--- frontend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/solace', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB error:", err));
